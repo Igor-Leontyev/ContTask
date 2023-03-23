7aa9355d27f9d514ac41e4a8a62568d6f9f26af2
Igor Leontyev <crybzleon@yandex.ru>
2023-03-23 21:49:33 +0300

    added img

--- main/Program.cs
+++ main/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+
+
+string[] CheckAlgo(string[] array, int value, int length = 0)
+{
+    string[] defArray = new string[length];
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= value)
+        {
+            if (defArray.Length > 0 && j < defArray.Length)
+            {
+                defArray[j] = array[i];
+                j++;
+            }
+            length++;
+        }
+    }
+
+    if (length > 0 && defArray.Length <= 0) return CheckAlgo(array, value, length);
+    return defArray;
+}
+
+string Show(string msg)
+{
+    Console.WriteLine(msg);
+    return Console.ReadLine() ?? String.Empty;
+}
+
+string[] CreateArray(int lenght)
+{
+    string[] arr = new string[lenght];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Show($"Введите любое значение в  {i}  элемент массива");
+    }
+    return arr;
+}
+
+int lenght = Convert.ToInt32(Show("Введите длину массива"));
+
+int value = Convert.ToInt32(Show("Введите необходимую длину элементов массива  "));
+
+string[] arr = CreateArray(lenght);
+
+Console.Write($"[{string.Join(", ", arr)}] -> [{string.Join(" , ", CheckAlgo(arr, value))}]");
